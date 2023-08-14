8f8b70ba39a473cc0700f2146e7e245622a18178
Christoffyw <weinhardtchristopher@gmail.com>
2023-08-14 14:43:18 -0400

    Update next.config.js

--- backend/scavenger_hunt_service/next.config.js
+++ backend/scavenger_hunt_service/next.config.js
@@ -4,9 +4,8 @@ const nextConfig = {
         return [
             {
                 // matching all API routes
-                source: "/:path*",
+                source: "/api/:path*",
                 headers: [
-                    { key: "Access-Control-Allow-Credentials", value: "false" },
                     { key: "Access-Control-Allow-Origin", value: "*" }, 
                     { key: "Access-Control-Allow-Methods", value: "GET,DELETE,PATCH,POST,PUT,OPTIONS" },
                     { key: "Access-Control-Allow-Headers", value: "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version" },
