67e36a3083247359fff0cbafb1583a6f654eecaf
Christoffyw <weinhardtchristopher@gmail.com>
2023-08-14 12:27:00 -0400

    fix CORS issue on POST

--- backend/scavenger_hunt_service/next.config.js
+++ backend/scavenger_hunt_service/next.config.js
@@ -6,9 +6,8 @@ const nextConfig = {
                 // matching all API routes
                 source: "/api/:path*",
                 headers: [
-                    { key: "Access-Control-Allow-Credentials", value: "false" },
                     { key: "Access-Control-Allow-Origin", value: "*" },
-                    { key: "Access-Control-Allow-Methods", value: "GET,DELETE,PATCH,POST,PUT" },
+                    { key: "Access-Control-Allow-Methods", value: "GET,OPTIONS,DELETE,PATCH,POST,PUT" },
                     { key: "Access-Control-Allow-Headers", value: "*" },
                 ]
             }
