5c809ab6f8e7aecebd6effa3cdabb8d47ba08ad4
Christoffyw <weinhardtchristopher@gmail.com>
2023-08-14 13:02:32 -0400

    fix.

--- backend/scavenger_hunt_service/next.config.js
+++ backend/scavenger_hunt_service/next.config.js
@@ -6,12 +6,19 @@ const nextConfig = {
             {
                 // matching all API routes
                 source: "/api/:path*",
-                destination: "https://positive-tahr-usually.ngrok-free.app/api/:path*",
                 headers: [
                     { key: "Access-Control-Allow-Origin", value: "*" },
                     { key: "Access-Control-Allow-Methods", value: "*" },
                     { key: "Access-Control-Allow-Headers", value: "*" },
                 ]
+            }
+        ]
+    }, 
+    async rewrites() {
+        return [
+            {        
+                source: "/api/:path*",
+                destination: "https://positive-tahr-usually.ngrok-free.app/api/:path*",
             }
         ]
     }
