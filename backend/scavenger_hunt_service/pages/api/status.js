4affacea5715ca839e5f5ba7b31d249671a3f96d
Christoffyw <weinhardtchristopher@gmail.com>
2023-08-15 13:45:31 -0400

    Update status.js

--- backend/scavenger_hunt_service/pages/api/status.js
+++ backend/scavenger_hunt_service/pages/api/status.js
@@ -1,4 +1,4 @@
-const fs = require('fs').promises;
+import axios from 'axios'
 
 export default async function handler(req, res) {
     if (req.method === 'OPTIONS') {
@@ -6,9 +6,10 @@ export default async function handler(req, res) {
     }
 
     setInterval(async () => {
-        const game_data_request = await fetch("/data/game.json");
-        let game_data = await game_data_request.json();
-        if(game_data.timer_started)
+        const { data } = await axios.get(
+            './data/game.json',
+        )
+        if(data.timer_started)
             return res.status(200).json({text:"Timer started!"});
     }, 500);
 }
