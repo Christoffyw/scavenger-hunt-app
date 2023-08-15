1a20295b3e680c0ed20d644b4f55a6796454f92e
Christoffyw <weinhardtchristopher@gmail.com>
2023-08-14 23:18:25 -0400

    Update next.config.js

--- backend/scavenger_hunt_service/next.config.js
+++ backend/scavenger_hunt_service/next.config.js
@@ -12,6 +12,16 @@ const nextConfig = {
                 ]
             }
         ]
+    },
+    webpack: (config, { isServer }) => {
+        // Fixes npm packages that depend on `fs` module
+        if (!isServer) {
+          config.node = {
+            fs: 'empty'
+          }
+        }
+    
+        return config
     }
 }
 
