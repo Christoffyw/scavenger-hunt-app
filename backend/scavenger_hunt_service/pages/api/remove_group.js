ee525c676640c13ab0f1e21b742f0e78229a4e2f
Christoffyw <weinhardtchristopher@gmail.com>
2023-08-15 02:00:10 -0400

    add missing variable

--- backend/scavenger_hunt_service/pages/api/remove_group.js
+++ backend/scavenger_hunt_service/pages/api/remove_group.js
@@ -7,6 +7,7 @@ export default async function handler(req, res) {
 
     let group_name = req.body.group_name;
 
+    let group_data = JSON.parse(await fs.readFile("./public/data/groups.json"));
     let group = group_data.find(group => group.group_name === group_name);
     if(group == undefined)
         return res.status(300).json({ text: 'A group with name: "' + group_name + '" does not exist' });
