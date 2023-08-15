e3ee515f89c9bd3be1689f61f0cdea3f6049d745
Christoffyw <weinhardtchristopher@gmail.com>
2023-08-15 13:55:00 -0400

    Update status.js

--- backend/scavenger_hunt_service/pages/api/status.js
+++ backend/scavenger_hunt_service/pages/api/status.js
@@ -1,11 +1,15 @@
+const fs = require('fs').promises;
+
+
 export default async function handler(req, res) {
     if (req.method === 'OPTIONS') {
         return res.status(202).send('Preflight ok');
     }
 
     setInterval(async () => {
-        var data = require('./public/data/game.json');
-        if(data.timer_started)
+        const game_data_request = await fs.readFile("./public/data/game.json");
+        let game_data = JSON.parse(game_data_request);
+        if(game_data.timer_started)
             return res.status(200).json({text:"Timer started!"});
-    }, 500);
+    }, 1000);
 }
