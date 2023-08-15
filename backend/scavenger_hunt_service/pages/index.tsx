25a0ed53ee1c6c9d29c70c88f9d2d6b5a342aabb
Christoffyw <weinhardtchristopher@gmail.com>
2023-08-15 00:05:16 -0400

    Update index.tsx

--- backend/scavenger_hunt_service/pages/index.tsx
+++ backend/scavenger_hunt_service/pages/index.tsx
@@ -7,6 +7,7 @@ export default function Page() {
         fetch('./data/groups.json')
           .then((res) => res.json())
           .then((resJson) => {
+            console.log(resJson);
             const data = JSON.parse(resJson);
             setGroupData(data);
         })
