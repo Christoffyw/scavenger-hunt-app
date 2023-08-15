c9d90f5ebe6608824e10cf5d2030b21ba00af38e
Christoffyw <weinhardtchristopher@gmail.com>
2023-08-15 01:05:42 -0400

    ok this looks cool

--- backend/scavenger_hunt_service/pages/index.tsx
+++ backend/scavenger_hunt_service/pages/index.tsx
@@ -1,41 +1,57 @@
 import React, { useState, useEffect } from "react";
+import {
+    useQuery,
+    useQueryClient,
+    QueryClient,
+    QueryClientProvider,
+} from '@tanstack/react-query'
+import axios from 'axios'
 
-export default function Page() {
-    const [group_data, setGroupData] = useState([]);
+const queryClient = new QueryClient()
 
-    useEffect(() => {
-        const intervalID = setInterval(() =>  {
-            fetch('./data/groups.json')
-            .then((res) => res.json())
-            .then((resJson) => {
-                setGroupData(resJson);
-            })
-        }, 1000);
+function Panel() {
     
-        return () => clearInterval(intervalID);
-      }, [])
-
-    const group_data_list = group_data.map(group =>
-        <div>
-            <h2>{group.group_name}</h2>
-            <div>
-                {group.posts.map(post => 
-                    <div>
-                        <p>Objective {post.objective_id}</p>
-                        <p>Submited At: {post.date}</p>
-                        <img src={"data:image/png;base64," + post.image_path} width="500"/>
-                    </div>
-                )}
-            </div>
-        </div>
-    );
+    const queryClient = useQueryClient()
+    const query = useQuery({
+        queryKey: ['groups'],
+        queryFn: async () => {
+          const { data } = await axios.get(
+            './data/groups.json',
+          )
+          return data
+        },
+    })
 
     return (
         <div>
+            {
+                query.data?.map(group =>
+                    <div key={group.group_name}>
+                        <h2>{group.group_name}</h2>
+                        <div>
+                            {group.posts.map(post => 
+                                <div key={post.objective_id}>
+                                    <p>Objective {post.objective_id}</p>
+                                    <p>Submited At: {post.date}</p>
+                                    <img src={"data:image/png;base64," + post.image_path} width="500"/>
+                                </div>
+                            )}
+                        </div>
+                    </div>
+                )  
+            }
+        </div>
+    ) 
+}
+
+export default function Page() {
+
+    return (
+        <QueryClientProvider client={queryClient}>
             <h1>Scavenger Hunt Control Panel</h1>
             <div>
-                { group_data_list }
+                <Panel />
             </div>
-        </div>
+        </QueryClientProvider>
     )
 }
